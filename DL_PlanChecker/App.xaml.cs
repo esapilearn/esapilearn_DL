dc52cfa394a3594b8e6a570ca0adf94c5f3d6701
Dong-Chang Lee <accord790902@hotmail.com>
2019-07-31 13:56:10 -0700

    First running version of ESAPIX

--- DL_PlanChecker/App.xaml.cs
+++ DL_PlanChecker/App.xaml.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using ESAPIX.Common;
+using ESAPIX.Common.Args;
 
 namespace DL_PlanChecker
 {
@@ -22,9 +23,9 @@ namespace DL_PlanChecker
             base.OnStartup(e);
             var bs = new AppBootstrapper<MainView>(() => { return VMS.TPS.Common.Model.API.Application.CreateApplication(); });
             //You can use the following to load a context (for debugging purposes)
-            //args = ContextIO.ReadArgsFromFile(@"context.txt");
+            //args = ContextIO.ReadArgsFromFile(@"C: \Users\cwalker\Desktop\context.txt");
             //Might disable (uncomment) for plugin mode
-            //bs.IsPatientSelectionEnabled = false;
+            bs.IsPatientSelectionEnabled = true;
             bs.Run(args);
         }
 
